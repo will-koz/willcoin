6d9d4a2b14cd9038d3fca4667df3159f13f49fd8
Will <slorkda@gmail.com>
2022-03-02 08:34:13 -0800

    Added new_wallet function to modular-ize

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 import discord
 import conf, will
 
-main_cs = will.Cryptosystem(size = conf.default_cryptosystem_size)
+main_cs = will.Cryptosystem(_size = conf.default_cryptosystem_size)
 # the main instance of the cryptosystem. Unless the bot is running multiple systems from the same
 # server, there does not need to be an array of cryptosystems
--- will.py
+++ will.py
@@ -6,20 +6,30 @@ class Wallet:
 	hash = ""
 	name = ""
 	seed = ""
-	def __init__ (self, creator = conf.anonymous, name = conf.default_wallet_name, players = None):
-		seed_prefix = creator + "-" + name + ":"
-		seed_suffix = str(int(time.time())) + "-" + wmath.stamp()
-		if players == None:
-			# if players are not passed, assume that it is okay to make a new wallet
-			hash = hashlib.sha256(bytes(seed_prefix + seed_suffix, conf.byte_encoding)).hexdigest()
-			print(seed_prefix + seed_suffix)
-			print(hash)
+	stamp = ""
+	create_time = ""
+	def __init__ (self,
+		_creator = conf.anonymous, _name = conf.default_wallet_name, _players = None):
+		if _players == None:
+			# if no players are passed, it is okay to assume that the wallet is okay to make
+			self.new_wallet(_creator = _creator, _name = _name, _players = _players)
+
+	def new_wallet (self,
+		_creator = conf.anonymous, _name = conf.default_wallet_name, _players = None):
+		creator = _creator
+		name = _name
+		create_time = str(int(time.time()))
+		stamp = wmath.stamp()
+		seed = creator + ":" + name + ":" + create_time + ":" + stamp
+		hash = hashlib.sha256(bytes(seed, conf.byte_encoding)).hexdigest()
+		print(seed)
+		print(hash)
 
 class Cryptosystem:
 	total_willcoin = 0
-	bank = Wallet(creator = conf.administration, name = conf.bank_name)
-	def __init__ (self, location = None, size = conf.default_cryptosystem_size):
-		self.new_cryptosystem(size)
+	bank = Wallet(_creator = conf.administration, _name = conf.bank_name)
+	def __init__ (self, _location = None, _size = conf.default_cryptosystem_size):
+		self.new_cryptosystem(_size)
 
-	def new_cryptosystem (self, size = conf.default_cryptosystem_size):
-		total_willcoin = size
+	def new_cryptosystem (self, _size = conf.default_cryptosystem_size):
+		total_willcoin = _size
