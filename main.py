1b3f8f26aadff7238bbb35b59ee11c09a24e19da
Will <slorkda@gmail.com>
2022-03-06 17:59:25 -0800

    Changed su_command to local_command_daemon to sound cooler

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ main_cs = will.Cryptosystem(_size = conf.default_cryptosystem_size)
 
 # Create a thread for parsing local commands
 
-def su_command (): # Super User command thread
+def local_command_daemon (): # Super User command thread
 	global exit_requested # Don't know why, but there needs to be a declaration of its globality.
 	while not exit_requested:
 		recieved_command = input()
@@ -19,5 +19,5 @@ def su_command (): # Super User command thread
 			recieved_command = recieved_command[1:] # if it's there.
 		exit_requested = will.exec_command(recieved_command, permissions = 1)
 
-local_thread = threading.Thread(target = su_command)
+local_thread = threading.Thread(target = local_command_daemon)
 local_thread.start()
